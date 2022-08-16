35f19e3aac727c3c678d4abfe6489395c19a8cbc
Doug Meurer <douggmeurer@gmail.com>
2022-08-16 17:51:59 -0300

    Done

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,22 +1,22 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+  
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+ 
   }
 
   getMinutes() {
-    // ... your code goes here
+ 
   }
 
   getSeconds() {
-    // ... your code goes here
+  
   }
 
   getCentiseconds() {
-    // ... your code goes here
+  
   }
 
   computeTwoDigitNumber(value) {
